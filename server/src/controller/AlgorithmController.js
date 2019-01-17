11e83b150edd4021893576c4396cb4c2b00e1cd2
Taylor Greeff <tgreeff@asu.edu>
2019-01-16 23:57:26 -0700

    Worked out functionality of algorithm api and will build further

--- server/src/controller/AlgorithmController.js
+++ server/src/controller/AlgorithmController.js
@@ -2,6 +2,7 @@
 
 //Add for later handling of Algorithm routing
 const AbstractController = require('./AbstractController');
+const ApiErrorModel = require('./../model/ApiErrorModel');
 const JsonModel = require('./../model/JsonModel');
 
 class AlgorithmController extends AbstractController{
@@ -9,11 +10,20 @@ class AlgorithmController extends AbstractController{
         return super.dispatch(false);
     }
 
-    getAction() {
-        let name = this.param('input', "");
-        if(name !== "") {
-            
+    getAction() { 
+                    
+        let id = this.param('id', ""); //query data from id to get exact algorithm
+        let name = this.param("name", ""); //query data from name to search algorithm
+        if(id === "" && name === "") {
+             return new ApiErrorModel(405, `method not allowed`);
         }
+
+        return new JsonModel(this.respsonse);
+    }
+
+    //Put in new algorithm
+    putAction() {
+        return new JsonModel(this.respsonse);
     }
 
     postAction() {
