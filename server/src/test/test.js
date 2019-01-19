9f75101427d2cca8fa3c8ecca91e60676ba2d6ef
Taylor Greeff <tgreeff@asu.edu>
2019-01-19 03:44:52 -0700

    Setup Error checking and organized test page. Added to server page. Added methods for creating a fetch request for test page.

--- server/src/test/test.js
+++ server/src/test/test.js
@@ -1,3 +1,31 @@
+function send(pressed) {
+    let url = document.URL + pressed;
+    let name = document.getElementById("name").value;
+    let id = document.getElementById("id").value;
+
+    //QUERY PARAMETERS
+    if(name !== "") {
+        console.log("NAME", name);
+        url += '?name=' + document.getElementById("name").value;
+    }
+    if (id !== "") {
+        console.log("ID", id);
+        url += '?id=' + document.getElementById("id").value;
+    }
+
+    //METHODS
+    if (document.getElementById("GET").checked) {
+        console.log("GET", url);
+        get(url);
+    }
+    else if (document.getElementById("POST").checked) {
+        console.log("POST");
+    } else {
+        console.log("PUT");
+    }
+    
+}
+
 async function post(url, data) {
     return fetch(url, {
         method: "POST",
@@ -10,10 +38,12 @@ async function post(url, data) {
             'Content-Type': 'application/json'
         }
     }).then((resp) => {
+        let error = document.getElementById("error");
         if (resp.ok) {
-            
+            error.innerHTML = "";
         } else {
-            
+            console.log(resp.status + ' ' + resp.statusText);
+            error.innerHTML = resp.status + ' ' + resp.statusText;
         }
     }).then((json) => {
         console.log(JSON.stringify(json));
@@ -32,13 +62,40 @@ async function get(url) {
             'Content-Type': 'application/json',
         }
     }).then((resp) => {
+        let error = document.getElementById("error");
         if (resp.ok) {
-            
+            error.innerHTML = "";
         } else {
-            
+            console.log(resp.status + ' ' + resp.statusText);
+            error.innerHTML = resp.status + ' ' + resp.statusText;
         }
     }).then((json) => {
         console.log(JSON.stringify(json));
         
+    });
+}
+
+async function put(url, data) {
+    return fetch(url, {
+        method: "PUT",
+        body: JSON.stringify({
+            data: data
+        }),
+        credentials: 'same-origin',
+        cache: "no-cache",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((resp) => {
+        let error = document.getElementById("error");
+        if (resp.ok) {
+            error.innerHTML = "";
+        } else {
+            console.log(resp.status + ' ' + resp.statusText);
+            error.innerHTML = resp.status + ' ' + resp.statusText;
+        }
+    }).then((json) => {
+        console.log(JSON.stringify(json));
+
     });
 }
