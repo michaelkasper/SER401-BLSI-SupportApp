5996a501f9d2ac651b2f62b3063906531806fc46
Michael Kasper <michael.kasper@partnersinleadership.com>
2019-02-01 08:54:37 -0700

    Added proxy system to help with building of client while backend is being built

---
+++ client/src/common/Environment.js
@@ -0,0 +1,10 @@
+export default {
+    USERNAME: process.env.REACT_APP_SAVE_USERNAME,
+    endpoint: {
+        SEARCH: '/sample-search.txt',
+        PERSON: '/sample-person.txt',
+        SAVE  : '/sample-save.txt',
+        // SEARCH: process.env.REACT_APP_SEARCH_ENDPONT,
+        // SAVE  : process.env.REACT_APP_SAVE_ENDPONT
+    }
+};
