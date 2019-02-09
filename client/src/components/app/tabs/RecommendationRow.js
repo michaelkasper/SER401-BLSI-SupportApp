a482d6f8d313e7c72f436fb4e89d278a6a7247d7
Michael Kasper <michael.kasper@partnersinleadership.com>
2019-02-09 10:54:49 -0700

    #224 Add drag and drop for recommendations

--- client/src/components/app/tabs/RecommendationRow.js
+++ client/src/components/app/tabs/RecommendationRow.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import Grid from "@material-ui/core/Grid";
 import {observer} from "mobx-react";
+import {withDrag} from "../../hoc/DragAndDrop";
+import withStyles from "@material-ui/core/es/styles/withStyles";
 
-const RecommendationRow = ({item: recommendation}) => {
+const RecommendationRow = ({item: recommendation, connectDragSource, classes}) => {
 
     return (
-        <Grid container
-              direction="row"
-              alignItems="center"
-        >
-            <Grid item xs={12} style={{
-                textAlign: 'left',
-            }}>
-                {recommendation.title}
-            </Grid>
-        </Grid>
+        connectDragSource(
+            <div className={classes.root}>
+                <Grid container
+                      direction="row"
+                      alignItems="center"
+                >
+                    <Grid item xs={12} style={{
+                        textAlign: 'left',
+                    }}>
+                        {recommendation.title}
+                    </Grid>
+                </Grid>
+            </div>
+        )
     );
 };
 
 
-export default observer(RecommendationRow);
+const styles = theme => ({
+    root: {
+        textAlign: 'right',
+        padding  : "10px",
+    },
+});
+
+
+export default withDrag('recommendation')(withStyles(styles)(observer(RecommendationRow)));
